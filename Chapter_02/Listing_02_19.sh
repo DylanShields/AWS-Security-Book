1900cbe54516f31c6143421581cd624d27c2ad04
Dylan Thomas Shields <dylanshields@fb.com>
2021-07-27 08:13:21 -0700

    Updates from Ch2 tech proof

--- Chapter_02/Listing_02_19.sh
+++ Chapter_02/Listing_02_19.sh
@@ -3,7 +3,7 @@
 # In order to create the master policy document, refer to tables 2.2, 2.3, and 2.4.
 aws iam create-policy \
     --policy-name IAMMasterPolicy \
-    --policy-document {REPLACE_WITH_MASTER_POLICY_DOCUMENT}
+    --policy-document file://Listing_02_19_master_policy_document.json
 aws iam attach-role-policy \
     --role-name IAMMaster \
     --policy-arn arn:aws:iam::123456789012:policy/IAMMasterPolicy
--- Chapter_02/Listing_02_20.sh
+++ Chapter_02/Listing_02_20.sh
@@ -3,7 +3,7 @@
 # In order to create the manager policy document, refer to tables 2.4 and 2.5.
 aws iam create-policy \
     --policy-name IAMManagerPolicy \
-    --policy-document {REPLACE_WITH_MANAGER_POLICY_DOCUMENT}
+    --policy-document file://Listing_02_20_manager_policy_document.json
 aws iam attach-role-policy \
     --role-name IAMManager \
     --policy-arn arn:aws:iam::123456789012:policy/IAMManagerPolicy
